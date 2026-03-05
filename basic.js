8ccddc48d29d37b03a25867b374ebc0b3a49220f
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2026-03-05 10:31:29 +0530

    classwork

--- basic.js
+++ basic.js
@@ -1,9 +1,33 @@
- let one = document.getElementById("id1")
+ function selectRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    
+    return color;
+}
 
- one.textContent = "Hello world! "
+// Get button elements
+const btnFirst = document.querySelector('.btn-first');
+const btnSecond = document.querySelector('.btn-second');
+const btnThird = document.querySelector('.btn-third');
 
- let two = document.querySelector("#id2")
-//  two.textContent = "New paragraph"
 
-let three = document.getElementById("id3")
-three.style.color = "red"
+btnFirst.addEventListener('click', function() {
+    document.body.style.backgroundColor = selectRandomColor();
+});
+
+// Change text color
+btnSecond.addEventListener('click', function() {
+    document.querySelector('.title').style.color = selectRandomColor();
+    document.querySelector('.text').style.color = selectRandomColor();
+});
+
+// Reset to default
+btnThird.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+    document.querySelector('.title').style.color = 'black';
+    document.querySelector('.text').style.color = 'black';
+});
