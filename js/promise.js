bdf5ec1f1277a907bc1d161c55a6167c05a250ab
Sagnik Ghosh <ghosh1234sagnik@gmail.com>
2026-04-18 00:17:15 +0530

    updated promise

--- js/promise.js
+++ js/promise.js
@@ -1,11 +1,14 @@
 fetch("https://api.github.com/usrs") //promise ke 3 state -> pending, fullfilled, reject
 
+// promise fullfilled
 .then((response)=>{ // convert json string to js object
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+
+    if (!response.ok) { 
+        throw new Error(`Data is not present in server`);
     }
     return response.json()
 })
+
 .then((data)=>{ //read , can acess js object properties
     
     const parent = document.getElementById('parent');
@@ -21,15 +24,30 @@ fetch("https://api.github.com/usrs") //promise ke 3 state -> pending, fullfilled
                                 }
 })
 
+// promise rejected
 .catch((error)=>{
-     const parent = document.getElementById('parent');
+    const parent = document.getElementById('parent');
     parent.textContent = error.message;
 })
 
-// whenever server responds the promise will be considered as fullfilled 
+// ** whenever server responds the promise will be considered as fullfilled 
 // like a typo in the url but even tho the server responds 
 
 // reject -> (server se baat hi nhi kar paya)
 // internet down
 // server down
 // DNS down
+
+
+// creating promise
+// const p1 = new Promise((resolve,reject)=>{
+//     resolve("hello");
+// })
+
+// // consuming promise
+// p1.then((response)=>{
+//     console.log(response);
+// })
+// p1.catch((error)=>{
+//     console.log(error)
+// })
